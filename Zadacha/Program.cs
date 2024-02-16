b25bd40fba606d6782be0aa08839bc79582b039f
Sbudanov <budanoff.2011@yandex.ru>
2024-02-16 11:18:02 +0400

    Ввод данных пользователем

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Добрый день. Прошу Вас указать, сколько значений вы введете");
+
+int num = Convert.ToInt32(Console.ReadLine ());
+
+Console.WriteLine("Теперь введите значения");
+
+string[] array = new string[num];
+for (int i = 0; i < num; i++)  
+    array[i]= Console.ReadLine (); 
+
+Console.WriteLine("Сейчас мы посмотрим, сколько вы ввели трехзначных элементов в наш массив");
+Console.WriteLine("Смотрите");
+
+void PrintArray(int[] array)                       //<--вывод данного массива на экран
+{
+    int count = array.Length;                      //Получение количества элементов 
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(NewArr);
