a249a6962c434d1b430581c29a0ba29b62223686
Sbudanov <budanoff.2011@yandex.ru>
2024-02-16 13:33:36 +0400

    Выполненная задача

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -2,25 +2,45 @@
 Console.WriteLine("Добрый день. Прошу Вас указать, сколько значений вы введете");
 
 int num = Convert.ToInt32(Console.ReadLine ());
+//  array [6, ABC, :-), 1234, complucter, .i.]
 
 Console.WriteLine("Теперь введите значения");
 
-string[] array = new string[num];
-for (int i = 0; i < num; i++)  
-    array[i]= Console.ReadLine ();
-            
 
-Console.WriteLine("Сейчас мы посмотрим, сколько вы ввели трехзначных элементов в наш массив");
-Console.WriteLine("Смотрите");
-int j = 0;
-for (int i = 0; i < num; i++)
-    if(array[i].Length <=3)
+string[] array = new string[num];
+int count = 0;
+for (int i = 0; i < num; i++)  
     {
-        array[j]= array[i];
-        j++;
+    array[i]= Console.ReadLine ();  
+    if (array[i].Length <=3)
+         {
+             count++;
+         }      
+    } 
+Console.WriteLine("Сейчас мы посмотрим, сколько вы ввели трехзначных элементов в наш массив");
+
+Console.WriteLine(count);
+
+Console.WriteLine("Смотрите");
+
+string[] arr = new string[count];
+for (int j = 0; j < count; )
+    {
+        for (int i = 0; i < num; i ++)
+        {
+            if(array[i].Length <=3)
+            {
+                arr[j]=array[i];
+                Console.Write(arr[j]);
+                 Console.Write(" ");
+                j++;
+            }
+             
+        }
+    
     }
 
-// for (int j = 0; j < num; j++)
+
 
 //     string Metod4(int count, string array[i]) //char C символ  (си)
 //         {
